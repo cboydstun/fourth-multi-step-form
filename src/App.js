a65be1ebb1747bee25681a014d0155dd5d2f362d
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-17 18:37:26 -0500

    add Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 import { UserForm } from './components/UserForm';
+import Navbar from './Navbar';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
+        <Navbar />
+
         <UserForm />
 
       </div>
