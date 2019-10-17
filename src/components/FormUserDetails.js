668919d5e8ce465eef6a7218a5e0ca6c8a50b08a
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-17 17:01:21 -0500

    reverted MuiThemeProvider import line
    changed back to include 'core'

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MuiThemeProvider } from '@material-ui/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import { AppBar, TextField, Button } from '@material-ui/core';
 
 export class FormUserDetails extends Component {
